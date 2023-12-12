6fe3e9735e309e3e8a844d781ecd15dcd19b003a
Fahim_Shams <pranjolfahim@gmail.com>
2023-12-12 16:11:24 +0000

    fixed receipt and order page

--- lib/ReceiptPage.dart
+++ lib/ReceiptPage.dart
@@ -9,45 +9,46 @@ class ReceiptPage extends StatelessWidget {
     final cartItems = context.watch<CartProvider>().cartItems;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Receipt'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            // Clear the cart and navigate to the home page
-            context.read<CartProvider>().clearCart();
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => MyHomePage()),
-              (route) => false,
-            );
-          },
+        appBar: AppBar(
+          title: Text('Receipt'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              // Clear the cart and navigate to the home page
+              context.read<CartProvider>().clearCart();
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+                (route) => false,
+              );
+            },
+          ),
         ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Receipt:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Receipt:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                // Display the items in the receipt
+                for (var item in cartItems)
+                  ListTile(
+                    title: Text(item.name),
+                    subtitle: Text('Price: \$${item.price.toStringAsFixed(2)}'),
+                  ),
+                SizedBox(height: 20),
+                Text(
+                  'Total Items: ${context.watch<CartProvider>().itemCount}',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
             ),
-            SizedBox(height: 10),
-            // Display the items in the receipt
-            for (var item in cartItems)
-              ListTile(
-                title: Text(item.name),
-                subtitle: Text('Price: \$${item.price.toStringAsFixed(2)}'),
-              ),
-            SizedBox(height: 20),
-            Text(
-              'Total Items: ${context.watch<CartProvider>().itemCount}',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
